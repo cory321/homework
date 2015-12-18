bd170ae33d2a819fbfad4869b8ac07d42fe6e5b4
cory321 <cory321@gmail.com>
2015-12-18 13:15:03 -0800

    ADD AngularJS

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-// (function(){
-// 	//js
-// })();
+
+(function(){
+	
+	angular.module("urbanYogaApp", [])
+
+	.controller("programsCtrl", function($scope, $http) {
+		$http.get('https://api.myjson.com/bins/5bdb3').then(function(response){
+    		$scope.programs = response.data;
+		});
+		$http.get('https://api.myjson.com/bins/17oy7').then(function(response){
+    		$scope.pricingOptions = response.data;
+		});
+	});
+
+})();
