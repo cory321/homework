e48bbb233b2611195cf51cb7d335cae342ce48ff
cory321 <cory321@gmail.com>
2015-12-19 16:20:12 -0800

    Removed Controller clutter

--- js/controllers.js
+++ js/controllers.js
@@ -3,31 +3,6 @@ angular.module("urbanYogaApp")
 .controller("programsCtrl", function($scope, $http) {
     $http.get('https://api.myjson.com/bins/5bdb3').then(function(response){
     	$scope.programs = response.data;
-
-        
-    	
-        var monthsArray = [];
-
-    	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
-    	var curYear, prevYear;
-
-    	for(j = 0; j < response.data.length; j++) {
-    		currYear = $scope.programs[j].Sales.CurrentYear;
-    		prevYear = $scope.programs[j].Sales.PreviousYear;
-
-    		for (i = 0; i < currYear.length; i++){
-    			// console.log(currYear[i]);
-    				monthsArray.push({
-    					month: monthNames[i],
-    					previousYear: prevYear[i],
-    					currentYear: currYear[i]
-    			});
-    		}
-    		// // currMonth = monthNames[];
-    		// console.log(program);
-    		// console.log(prevYear+ " and " + currYear);
-    	}
-    	$scope.months = monthsArray;
     });
 
     $http.get('https://api.myjson.com/bins/17oy7').then(function(response){
