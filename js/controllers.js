5ce390b2a69916c5100f5ecf9b99c2946cfd0af3
cory321 <cory321@gmail.com>
2015-12-21 16:15:20 -0800

    Added AJAX preloader for API calls

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,22 @@
 angular.module("urbanYogaApp")
 
 .controller("programsCtrl", function($scope, $http) {
+    
+    $scope.pData = {};
+    $scope.poData = {};
+
+    $scope.pData.dataLoading = true;
+    $scope.poData.dataLoading = true;
+
     $http.get('https://api.myjson.com/bins/5bdb3').then(function(response){
     	$scope.programs = response.data;
-    });
+    }).finally(function () {
+    	$scope.pData.dataLoading = false;
+	});
 
     $http.get('https://api.myjson.com/bins/17oy7').then(function(response){
     	$scope.pricingOptions = response.data;
-    });
+    }).finally(function () {
+    	$scope.poData.dataLoading = false;
+	});
 });
