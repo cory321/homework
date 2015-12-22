fc9d99b52f823d09215d5489721818cbc617154c
cory321 <cory321@gmail.com>
2015-12-21 17:01:03 -0800

    updated controller semantics

--- js/controllers.js
+++ js/controllers.js
@@ -8,15 +8,19 @@ angular.module("urbanYogaApp")
     $scope.programsData.dataLoading = true;
     $scope.pricingOptions.dataLoading = true;
 
-    $http.get('https://api.myjson.com/bins/5bdb3').then(function(response){
-    	$scope.programs = response.data;
-    }).finally(function () {
-    	$scope.programsData.dataLoading = false;
-	});
+    $http.get('https://api.myjson.com/bins/5bdb3')
+         .then(function(response){
+        	 $scope.programs = response.data;
+         })
+         .finally(function () {
+         	 $scope.programsData.dataLoading = false;
+    	 });
 
-    $http.get('https://api.myjson.com/bins/17oy7').then(function(response){
-    	$scope.pricingOptions = response.data;
-    }).finally(function () {
-    	$scope.pricingOptions.dataLoading = false;
-	});
+    $http.get('https://api.myjson.com/bins/17oy7')
+         .then(function(response){
+        	 $scope.pricingOptions = response.data;
+         })
+         .finally(function () {
+        	 $scope.pricingOptions.dataLoading = false;
+    	 });
 });
